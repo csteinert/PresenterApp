882c708da3d06567c239a99a5703501291366d8f
alecdeitz <alecdeitz@gmail.com>
2014-07-24 14:02:56 -0400

    Merge conflict

--- Presenter.js
+++ Presenter.js
@@ -11,6 +11,24 @@ $(document).ready(function () {
             var blah = QuestionInfoTemplate(this);
             $("#section-question-info").append(blah);
         });
+
+        var myDataRef = new Firebase('https://opl9mlhj485.firebaseio-demo.com/');
+        $('#messageInput').keypress(function (e) {
+            if (e.keyCode == 13) {
+                var name = $('#nameInput').val();
+                var text = $('#messageInput').val();
+                myDataRef.push({ name: name, text: text });
+                $('#messageInput').val('');
+            }
+        });
+        myDataRef.on('child_added', function (snapshot) {
+            var message = snapshot.val();
+            displayChatMessage(message.name, message.text);
+        });
+        function displayChatMessage(name, text) {
+            $('<div/>').text(text).prepend($('<em/>').text(name + ': ')).appendTo($('#messagesDiv'));
+            $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+        };
     });
 
     function QuestionInfoTemplate(questionInfo) {
