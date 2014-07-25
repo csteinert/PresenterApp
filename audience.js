57e5976c01f1111658c4eb6d7b347f7d25b2658d
Philip Jordan <philip.jordan@live.com>
2014-07-25 10:23:52 -0400

    Changes to audience.js

--- audience.js
+++ audience.js
@@ -1,45 +1,33 @@
-var myDataRef = new Firebase('https://atndemo1.firebaseio.com/');
-
 $(document).ready(function () {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    var myDataRef = new Firebase('https://atndemo1.firebaseio.com/SelectedQuestion/');
+    myDataRef.on('child_added', function (snapshot) {
+        var message = snapshot.val();
+        updateQuestion(message.id);
+    });
+    function updateQuestion(id) {
+        if (id != 'blank' && id != 'qa')
+        {
+            $.getJSON('Questions.json', function (json) {
+                var selectedQuestion;
+                $.each(json, function () {
+                    var currentQuestion = this;
+                    if (currentQuestion.id == id)
+                    {
+                        var answersInfo = "";
+                        $.each(currentQuestion.answers, function () {
+                            answersInfo += "<li>" + this + "</li>"
+                        });
+                        var questionHtml = "<article><h3>" +
+                            currentQuestion.question +
+                            "</h3>" +
+                            "<ol>" +
+                            answersInfo +
+                            "</ol>"
+                            "</article>";
+                        $("#section-selected-question").html(questionHtml);
+                    }
+                });
+            });
+        }
+    };
 });
